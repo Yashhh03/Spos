a0195fba1e7b54c5307acce049f05e53abd86eec
Yash Suryawanshi <114762510+Yashhh03@users.noreply.github.com>
2023-11-17 18:10:46 +0530

    Create FirstFit.java

---
+++ P4/FirstFit.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class FirstFit {
+    public static void firstFit(int[] blockSize, int m, int[] processSize, int n) {
+        int[] allocation = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (blockSize[j] >= processSize[i]) {
+                    allocation[i] = j;
+                    blockSize[j] -= processSize[i];
+                    break;
+                }
+            }
+        }
+
+        System.out.println("\nProcess No.\tProcess Size\tBlock no.");
+        for (int i = 0; i < n; i++) {
+            System.out.print(" " + (i + 1) + "\t\t" + processSize[i] + "\t\t");
+            if (allocation[i] != -1)
+                System.out.print(allocation[i] + 1);
+            else
+                System.out.print("Not Allocated");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of memory blocks: ");
+        int m = scanner.nextInt();
+        int[] blockSize = new int[m];
+
+        System.out.println("Enter the memory block sizes:");
+        for (int i = 0; i < m; i++) {
+            blockSize[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter the number of processes: ");
+        int n = scanner.nextInt();
+        int[] processSize = new int[n];
+
+        System.out.println("Enter the process sizes:");
+        for (int i = 0; i < n; i++) {
+            processSize[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        System.out.println("\nFirst Fit Memory Allocation \n");
+        firstFit(blockSize, m, processSize, n);
+    }
+}
